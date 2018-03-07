db03e053799c80eb5f0bb1bbe2e90c91f6aa3ef3
ukind <ukind@users.noreply.github.com>
2018-03-07 09:36:00 +0700

    connect to cloud
    MEN - connect to cloud database (MongoDB) / no local database

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-var express = require('express');
-var request = require('request');
-var bodyParser = require('body-parser');
-
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const mongoClient = require('mongodb').MongoClient;
 const bootstrapJS = '/node_modules/bootstrap/dist/js';
 const bootstrapCSS = '/node_modules/bootstrap/dist/css';
+let db;
 
 let dataArray = [];
-// let registeredCamper = {
-//   name: {first: '', last: ''},
-//   picture: {large: ''}
-// };
-
 var camper = 'https://randomuser.me/api/?results=20';
 
 var app = express();
@@ -18,21 +14,42 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/user', express.static(__dirname + '/public'));
 app.use('/vendor', express.static(__dirname + bootstrapJS));
 app.use('/vendor', express.static(__dirname + bootstrapCSS));
-
 app.set('view engine', 'ejs');
+
+//selecting database: yelpcamp {not collection}
+mongoClient.connect(
+  'mongodb://ukind:kagekuman@ds253468.mlab.com:53468/yelpcamp',
+  function(error, client) {
+  if (error) {
+    return console.log(error);
+  }
+  db = client.db('yelpcamp');
+
+  app.listen(12345, process.env.IP, () => {
+    // clearing console
+    console.log('\x1Bc');
+    console.log('server started');
+  });
+});
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 app.get('/camper', function(req, res) {
-
   request(camper, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       if (dataArray.length === 0) {
         const Data = JSON.parse(body);
         Data.results.forEach(elemen => {
           dataArray.push(elemen);
+          // creating collection in database named camper
+          db.collection('camper').save(elemen, (error, result) => {
+            if (error) {
+              return console.log(error);
+            }
+            console.log('data saved');
+          });
         });
       }
       res.render('camper', {camperHTML: dataArray});
@@ -47,6 +64,13 @@ app.post('/camper', function(req, res) {
   const registeredCamper = {};
   registeredCamper.name = {first: camperNameFirst, last: camperNameLast};
   registeredCamper.picture = {large: imageUrl || ''};
+  // creating collection in database named camper
+  db.collection('camper').save(registeredCamper, (error, result) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('data saved');
+  });
   dataArray.unshift(registeredCamper);
   res.redirect('/camper');
 
@@ -58,10 +82,4 @@ app.get('/camper/new', function(req, res) {
 
 app.get('*', (req, res) => {
   res.send('page not found');
-});
-
-app.listen(12345, process.env.IP, () => {
-  // clearing console
-  console.log('\x1Bc');
-  console.log('server started');
 });
