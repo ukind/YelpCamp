b150ba9a148c3d46f0095471cdfc3f6733a0566c
ukind <ukind@users.noreply.github.com>
2018-03-08 10:37:01 +0700

    noncloud version fixed connect ot local db

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const request = require('request');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const bootstrapJS = '/node_modules/bootstrap/dist/js';
-const bootstrapCSS = '/node_modules/bootstrap/dist/css';
-let db;
+const express       = require('express');
+const request       = require('request');
+const bodyParser    = require('body-parser');
+const mongoose      = require('mongoose');
+const bootstrapJS   = '/node_modules/bootstrap/dist/js';
+const bootstrapCSS  = '/node_modules/bootstrap/dist/css';
 
 let dataArray = [];
-var camper = 'https://randomuser.me/api/?results=20';
+var camper    = 'https://randomuser.me/api/?results=20';
+var app       = express();
 
-var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/user', express.static(__dirname + '/public'));
 app.use('/vendor', express.static(__dirname + bootstrapJS));
 app.use('/vendor', express.static(__dirname + bootstrapCSS));
 app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost/yelpcamp');
+
+// database schemas
+const camperSchema = new mongoose.Schema({
+  name: {first: String, last: String},
+  picture: {large: String}
+});
+
+const camperDatabase = mongoose.model('camper', camperSchema);
 
 app.get('/', (req, res) => {
   res.render('landing');
@@ -23,13 +31,22 @@ app.get('/', (req, res) => {
 app.get('/camper', function(req, res) {
   request(camper, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      if (dataArray.length === 0) {
-        const Data = JSON.parse(body);
-        Data.results.forEach(elemen => {
-          dataArray.push(elemen);
+      // detect empty collection indatabase
+      camperDatabase.count({}, function(error, count) {
+        if (count === 0) {
+          const Data = JSON.parse(body);
+          Data.results.forEach(elemen => {
+            camperDatabase.create({
+              name: {first: elemen.name.first, last: elemen.name.last},
+              picture: {large: elemen.picture.large}
+            });
+          });
+        }
+        // show data from database
+        camperDatabase.find({}, function(error, data) {
+          res.render('camper', {camperHTML: data});
         });
-      }
-      res.render('camper', {camperHTML: dataArray});
+      });
     }
   });
 });
@@ -39,9 +56,18 @@ app.post('/camper', function(req, res) {
   const camperNameLast = req.body.camperNameLast;
   const imageUrl = req.body.camperImage;
   const registeredCamper = {};
-  registeredCamper.name = {first: camperNameFirst, last: camperNameLast};
-  registeredCamper.picture = {large: imageUrl || ''};
-  dataArray.unshift(registeredCamper);
+  // registeredCamper.name = {first: camperNameFirst, last: camperNameLast};
+  // registeredCamper.picture = {large: imageUrl || ''};
+  // dataArray.unshift(registeredCamper);
+  camperDatabase.create({
+    name: {first: camperNameFirst, last: camperNameLast},
+    picture: {large: imageUrl || ''}
+  }, function(error, result) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(result);
+  });
   res.redirect('/camper');
 
 });
---
+++ public/index.js
@@ -0,0 +1,1 @@
+const camperUser = document.querySelectorAll('.camperUser');
