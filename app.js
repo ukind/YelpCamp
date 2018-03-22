62575997bb378da42ef2412004a28f1ff01b0225
ukind <ukind@users.noreply.github.com>
2018-03-22 10:58:05 +0700

    split routes

--- app.js
+++ app.js
@@ -6,14 +6,20 @@ const passport      = require('passport');
 const localStrategy = require('passport-local');
 
 // CUSTOM REQUIRE
-const CamperInterface = require('./models/camperDatabaseInterface');
+
 const GetCamperFromDatabaseJSON = require('./models/seedDBJSON');
 const SeedDB = require('./models/seedDB');
-const comment = require('./models/comments');
-const camperCounter = require('./models/camperCounter');
 const camperCollection = require('./models/user');
+const camperCounter = require('./models/camperCounter');
+const CamperInterface = require('./models/camperDatabaseInterface');
 const bootstrapJS   = './node_modules/bootstrap/dist/js';
 const bootstrapCSS  = './node_modules/bootstrap/dist/css';
+
+// ROUTES
+const commentsRoutes = require('./routes/comments');
+const authRoutes = require('./routes/auth');
+const camperRoutes = require('./routes/camper');
+
 
 var app       = express();
 
@@ -46,7 +52,7 @@ function calculateCamper() {
   });
 };
 
-// PASSING VARIABLE GLOBALLY IN EPRESS TO EJS
+// PASSING VARIABLE GLOBALLY IN EXPREASS TO EJS
 app.use(function(req, res, next) {
   calculateCamper();
   res.locals.camperCounterHTML = sumCamper;
@@ -69,60 +75,6 @@ app.get('/', (req, res) => {
   res.redirect('/camper');
 });
 
-// ROUTE: CAMPER
-app.get('/camper',  function(req, res) {
-  // GetCamperFromDatabaseJSON();
-  // SeedDB.removeAllCamper();
-  // SeedDB.camperCreator();
-  console.log(req.user);
-
-  CamperInterface.count({}, function(error, count) {
-    if (count >= 0) {
-      CamperInterface.find({}, function(error, data) {
-
-          res.render('./camper/camper', {camperHTML: data});
-        });
-    }
-  });
-
-});
-
-// ROUTES: NEW CAMPER
-app.post('/camper', function(req, res) {
-  const newUser = new camperCollection({username: req.body.username});
-  const password = req.body.password;
-  const camperNameFirst = req.body.firstName;
-  const camperNameLast = req.body.lastName;
-  const imageUrl = req.body.imageURL;
-  var empty;
-  camperCollection.register(newUser, password, (error, user) => {
-    if (error) {
-      console.log(error);
-      return res.render('./camper/register');
-    }
-    passport.authenticate('local')(req, res, () => {
-      // console.log(res);
-      CamperInterface.create({
-        name: {first: camperNameFirst, last: camperNameLast},
-        picture: {large: imageUrl || 'https://pingendo.com/assets/photos/wireframe/photo-1.jpg'},
-        gender: empty || 'Not available',
-        email: empty || 'Not available',
-        location: {
-          street: empty || 'Not available',
-          city: empty || 'Not available',
-          state: empty || 'Not available',
-          postcode: empty || 'Not available'
-        }
-      }, function(error, result) {
-        if (error) {
-          console.log(error);
-        }
-        res.redirect('/camper');
-      });
-    });
-  });
-});
-
 // ROUTE: GET FORM
 app.get('/camper/new', function(req, res) {
 
@@ -141,33 +93,6 @@ app.get('/camper/:id', function(req, res) {
 
       res.render('./camper/show', {camperIDHtml: result});
     });
-});
-
-// ROUTE: GET COMMENT CAMPER
-app.get('/camper/:id/comments/new', isLoggedIn, (req, res) => {
-  const camperID = req.params.id;
-  CamperInterface.findById(camperID, function(err, result) {
-
-    res.render('./comment/new', {camperComment: result});
-  });
-
-});
-
-// ROUTE: NEW COMMENT CAMPER
-app.post('/camper/:id/comment', isLoggedIn, (req, res) => {
-  const camperID = req.params.id;
-  CamperInterface.findById(camperID, (error, camper) => {
-    if (error) {
-      console.log(error);
-      res.redirect('/camper');
-    }
-    const commentContainer = req.body.comment;
-    comment.create(commentContainer, (error, result) => {
-      camper.comments.push(result);
-      camper.save();
-      res.redirect('/camper/' + camper._id);
-    });
-  });
 });
 
 // ROUTE: REGISTER
@@ -190,39 +115,12 @@ app.post('/camper/:id/comment', isLoggedIn, (req, res) => {
 //   });
 // });
 
-// ROUTE : LOGIN
 
-app.get('/login', (req, res) => {
-  calculateCamper();
-  res.render('./camper/login');
-});
+// USE SPLITTED ROUTER
 
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/camper',
-  failureRedirect: '/login'
-}), (req, res) => {
-
-});
-
-// FUNCION: detect session
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-}
-
-
-
-// ROUTE: LOGOUT
-app.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/camper');
-});
-
-app.get('*', (req, res) => {
-  res.send('page not found');
-});
+app.use(commentsRoutes);
+app.use(camperRoutes);
+app.use(authRoutes);
 
 app.listen(12345, '127.0.0.1', () => {
   // clearing console
